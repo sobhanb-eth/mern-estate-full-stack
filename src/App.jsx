6a6d4198a38c9b15acf95a8b98745c9a8c60b21e
sbahrami-vz <sobhan.bahrami@viziverse.com>
2023-11-04 00:12:09 +0100

    feat: create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
 
 export default function App() {
-  return <h1 className="text-red-400">App</h1>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp/>} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/about" element={<About />} />
+  </Routes>
+  </BrowserRouter>;
 }
