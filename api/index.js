80a7b2bea83e6242f3ad4f96528331f8e517f794
sobhanb-eth <ednqor@inf.elte.hu>
2023-11-05 13:04:53 +0100

    feat: initial database connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is Running on Port 3000 !");
