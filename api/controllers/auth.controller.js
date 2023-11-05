a8228b2df2614614188f48264acdd1bf2b4ef14e
sobhanb-eth <ednqor@inf.elte.hu>
2023-11-05 15:44:19 +0100

    feat: error handling middleware util function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
@@ -11,7 +11,7 @@ export const signup = async (req, res) => {
         res.status(201).json("User created successfully!");
     } catch (error)
     {
-        res.status(500).json(error.message);
+        next(error);
     }
     
 };
